c8eec0b8974e68b24f5dd02dda627eee1c49e3d4
hsinnan75 <arith@iis.sinica.edu.tw>
2019-10-01 09:51:48 +0800

    modified:   run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,14 +4,14 @@ echo
 echo "Test1 -- Generate index files with a reference file"
 echo "Command=bin/bwt_index test/ref.fa test/RefIdx"
 echo
-./bwt_index test/ref.fa test/RefIdx
+bin/bwt_index test/ref.fa test/RefIdx
 
 #test alignment
 echo
 echo "Test2 -- Align paired-end reads with 4 threads"
 echo "Command=bin/MapCaller -i test/RefIdx -t 4 -f test/r1.fq -f2 test/r2.fq -vcf test/out.vcf"
 echo
-./MapCaller -i test/RefIdx -t 4 -f test/r1.fq -f2 test/r2.fq -vcf test/out.vcf
+bin/MapCaller -i test/RefIdx -t 4 -f test/r1.fq -f2 test/r2.fq -vcf test/out.vcf
 
 echo
 echo "[End of test]"
